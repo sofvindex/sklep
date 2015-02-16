de103108f78d75329ff4fb6637496e1e5f1af2cb
Wspolne konto <GrannyTS@gmail.com>
2015-02-16 23:31:14 +0100

    Wygenerowoano Enitity Purchase

---
+++ src/SklepBundle/Entity/Purchase.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace SklepBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Purchase
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="SklepBundle\Entity\PurchaseRepository")
+ */
+class Purchase
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="time", type="datetime")
+     */
+    private $time;
+
+    /**
+     * @var \stdClass
+     *
+     * @ORM\Column(name="movies", type="object")
+     */
+    private $movies;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Purchase
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Purchase
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set time
+     *
+     * @param \DateTime $time
+     * @return Purchase
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    /**
+     * Get time
+     *
+     * @return \DateTime 
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * Set movies
+     *
+     * @param \stdClass $movies
+     * @return Purchase
+     */
+    public function setMovies($movies)
+    {
+        $this->movies = $movies;
+
+        return $this;
+    }
+
+    /**
+     * Get movies
+     *
+     * @return \stdClass 
+     */
+    public function getMovies()
+    {
+        return $this->movies;
+    }
+}
