cace039fc3e47399559a3c7dbb6a69e7af519de7
Wspolne konto <GrannyTS@gmail.com>
2015-02-16 23:54:33 +0100

    Generacja wszystkich plików przy użyciu http://symfony.com/doc/current/bundles/SensioGeneratorBundle/commands/generate_doctrine_crud.html

---
+++ src/SklepBundle/Form/MovieType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SklepBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MovieType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('rating')
+            ->add('price')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SklepBundle\Entity\Movie'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sklepbundle_movie';
+    }
+}
---
+++ src/SklepBundle/Form/ReviewType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SklepBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ReviewType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('autor')
+            ->add('comment')
+            ->add('rating')
+            ->add('movie')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SklepBundle\Entity\Review'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sklepbundle_review';
+    }
+}
